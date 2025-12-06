0a922a68a15c115a6f5631978ca71d9929cf8636
Rurutia1027 <rurutia1027@gmail.com>
2025-12-06 23:03:54 +0800

    update migration sql

--- customer/src/main/resources/db/migration/V1__create_customer_table.sql
+++ customer/src/main/resources/db/migration/V1__create_customer_table.sql
@@ -1,7 +1,9 @@
 -- V1__create_customer_table.sql
-CREATE TABLE customers (
-                           id BIGSERIAL PRIMARY KEY,
-                           name VARCHAR(100) NOT NULL,
-                           email VARCHAR(255) NOT NULL UNIQUE,
-                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE SEQUENCE customer_id_sequence START 1;
+
+CREATE TABLE customer (
+                          id BIGINT DEFAULT nextval('customer_id_sequence') PRIMARY KEY,
+                          first_name VARCHAR(50) NOT NULL,
+                          last_name VARCHAR(50) NOT NULL,
+                          email VARCHAR(100) NOT NULL
 );
