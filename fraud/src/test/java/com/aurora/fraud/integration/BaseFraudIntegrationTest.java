fcc104528edc8e980b50278d1ade24beada00a57
Rurutia1027 <rurutia1027@gmail.com>
2025-12-13 20:43:10 +0800

    add fraud integraiont test cases via test containers

---
+++ fraud/src/test/java/com/aurora/fraud/integration/BaseFraudIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.aurora.fraud.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+        "spring.flyway.eanbled=false",
+        "spring.cloud.consul.enabled=false",
+        "spring.cloud.consul.discovery.enabled=false"
+})
+public class BaseFraudIntegrationTest {
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("fraud")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void registryDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+}
---
+++ fraud/src/test/java/com/aurora/fraud/integration/FraudServiceIT.java
@@ -0,0 +1,48 @@
+package com.aurora.fraud.integration;
+
+import com.aurora.clients.fraud.FraudCheckResponse;
+import com.aurora.fraud.FraudCheckHistory;
+import com.aurora.fraud.FraudCheckHistoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FraudServiceIT extends BaseFraudIntegrationTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    @Test
+    void shouldReturnFraudCheckAndPersistHistory() {
+        Integer customerId = UUID.randomUUID().variant();
+
+        // Call controller endpoint
+        ResponseEntity<FraudCheckResponse> response = restTemplate.getForEntity(
+                "/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customerId);
+
+        // Assert controller response
+        assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getIsFraudster()).isFalse();
+
+
+        // Assert DB persistence
+        List<FraudCheckHistory> histories =
+                fraudCheckHistoryRepository.findAll();
+        assertThat(histories.size()).isGreaterThanOrEqualTo(1);
+        FraudCheckHistory history = histories.get(0);
+        assertThat(history.getCreatedAt()).isNotNull();
+        assertThat(history.getCustomerId()).isEqualTo(customerId);
+        assertThat(history.getIsFraudster()).isEqualTo(false);
+    }
+}
